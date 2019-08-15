f89fd89ab4cca0e14ee3da9d7062ab0697abc31d
furuiqing <214943778@QQ.com>
2019-08-15 19:58:47 +0800

    first commit

---
+++ goober's life/pages/death/death.js
@@ -0,0 +1,23 @@
+// pages/death/death.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    animationRotate: {}
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
